f106b3400ddd28a3bc76bb037e5e954968936ad5
Emily Pan <pancakerula@gmail.com>
2025-04-21 11:31:49 -0700

    dinosaur

--- src/main/java/APLine.java
+++ src/main/java/APLine.java
@@ -1,5 +1,16 @@
 public class APLine
 {
-  /* your code here */
+  private int a, b, c;
+  public APLine(int ai, int bi, int ci){
+    a = ai;
+    b = bi;
+    c = ci;
+  }
+  public double getSlope(){
+    return ((double)a*(-1))/b;
+  }
+  public boolean isOnline(int x, int y){
+    return a*x+b*y+c==0;
+  }
   
 }
